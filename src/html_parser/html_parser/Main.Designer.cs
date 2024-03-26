f2f6a699c98951d2c28103153b17a0c465556c98
efgrthyjkuilo <147861361+1054009@users.noreply.github.com>
2024-03-26 19:42:57 -0400

    Add parsing thanks to AngleSharp

--- src/html_parser/html_parser/Main.Designer.cs
+++ src/html_parser/html_parser/Main.Designer.cs
@@ -29,10 +29,10 @@
 		private void InitializeComponent()
 		{
 			this.label1 = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.InputBox = new System.Windows.Forms.TextBox();
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			this.panel1 = new System.Windows.Forms.Panel();
-			this.button1 = new System.Windows.Forms.Button();
+			this.ConvertButton = new System.Windows.Forms.Button();
 			this.tableLayoutPanel1.SuspendLayout();
 			this.panel1.SuspendLayout();
 			this.SuspendLayout();
@@ -46,16 +46,18 @@
 			this.label1.TabIndex = 1;
 			this.label1.Text = "HTML Input";
 			// 
-			// textBox1
+			// InputBox
 			// 
-			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this.InputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.textBox1.Location = new System.Drawing.Point(0, 22);
-			this.textBox1.Multiline = true;
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(610, 347);
-			this.textBox1.TabIndex = 0;
+			this.InputBox.Location = new System.Drawing.Point(0, 22);
+			this.InputBox.Multiline = true;
+			this.InputBox.Name = "InputBox";
+			this.InputBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.InputBox.Size = new System.Drawing.Size(610, 347);
+			this.InputBox.TabIndex = 0;
+			this.InputBox.WordWrap = false;
 			// 
 			// tableLayoutPanel1
 			// 
@@ -63,7 +65,7 @@
 			this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			this.tableLayoutPanel1.ColumnCount = 1;
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-			this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.ConvertButton, 0, 1);
 			this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
 			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
@@ -80,24 +82,25 @@
 			this.panel1.AutoSize = true;
 			this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			this.panel1.Controls.Add(this.label1);
-			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.InputBox);
 			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.panel1.Location = new System.Drawing.Point(7, 7);
 			this.panel1.Name = "panel1";
 			this.panel1.Size = new System.Drawing.Size(610, 369);
 			this.panel1.TabIndex = 3;
 			// 
-			// button1
+			// ConvertButton
 			// 
-			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this.ConvertButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.button1.Location = new System.Drawing.Point(7, 382);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(610, 52);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "Convert to CSV";
-			this.button1.UseVisualStyleBackColor = true;
+			this.ConvertButton.Location = new System.Drawing.Point(7, 382);
+			this.ConvertButton.Name = "ConvertButton";
+			this.ConvertButton.Size = new System.Drawing.Size(610, 52);
+			this.ConvertButton.TabIndex = 0;
+			this.ConvertButton.Text = "Convert to CSV";
+			this.ConvertButton.UseVisualStyleBackColor = true;
+			this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
 			// 
 			// Main
 			// 
@@ -121,9 +124,9 @@
 		#endregion
 
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox InputBox;
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
 		private System.Windows.Forms.Panel panel1;
-		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button ConvertButton;
 	}
 }
--- src/html_parser/html_parser/Main.cs
+++ src/html_parser/html_parser/Main.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using System.Windows.Forms;
 
 namespace html_parser
@@ -13,6 +19,85 @@ namespace html_parser
 		private void Main_Load(object sender, EventArgs e)
 		{
 
+		}
+
+		private void ConvertButton_Click(object sender, EventArgs e)
+		{
+			string HTML = InputBox.Text;
+			if (HTML.Length < 1) return;
+
+			HtmlParser Parser = new HtmlParser();
+			IHtmlDocument Document = Parser.ParseDocument(HTML);
+
+			IElement Table = Document.QuerySelector("table");
+			if (Table == null) return;
+
+			List<string> ColumnHeaders = new List<string>();
+			List<string> Rows = new List<string>();
+
+			// Get all the DOM data
+			foreach (IElement Row in Table.QuerySelectorAll("tr"))
+			{
+				// Assume this is the header row
+				if (Row.QuerySelector("th") != null)
+				{
+					ColumnHeaders.Clear();
+
+					foreach (IElement TableHeader in Row.QuerySelectorAll("th"))
+						ColumnHeaders.Add(TableHeader.InnerHtml);
+
+					continue;
+				}
+
+				// Handle rows
+				List<string> CurrentRow = new List<string>();
+
+				foreach (IElement RowData in Row.QuerySelectorAll("td"))
+					CurrentRow.Add(RowData.InnerHtml);
+
+				Rows.Add(string.Join(",", CurrentRow));
+			}
+
+			// Turn it into something useful
+			string CSV = string.Format("{0}\n{1}", string.Join(",", ColumnHeaders), string.Join("\n", Rows));
+			if (CSV.Length < 1)
+			{
+				MessageBox.Show("No data to write!");
+				return;
+			}
+
+			// Save it!
+			// Very organized code
+			SaveFileDialog Dialog = new SaveFileDialog()
+			{
+				Filter = "Comma Separated File|*.csv",
+				Title = "Choose CSV location"
+			};
+
+			Dialog.ShowDialog();
+
+			if (Dialog.FileName.Length > 0)
+			{
+				using (FileStream CSVStream = (FileStream)Dialog.OpenFile())
+				{
+					if (!CSVStream.CanWrite)
+					{
+						MessageBox.Show("Unable to write file!");
+						return;
+					}
+
+					try
+					{
+						byte[] Data = (new UTF8Encoding(true)).GetBytes(CSV);
+
+						CSVStream.Write(Data, 0, Data.Length);
+					}
+					catch (Exception)
+					{
+						MessageBox.Show("Failed to write file!");
+					}
+				}
+			}
 		}
 	}
 }
