a5b13bbb5d55c9f1581c8951676126f660c2b300
Zach <147861361+1054009@users.noreply.github.com>
2024-03-26 14:09:19 -0400

    Create StringCleaner

---
+++ src/csv_importer/csv_importer/StringCleaner.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace csv_importer
+{
+	public class StringCleaner
+	{
+		private Regex FileNameCleaner;
+		private Regex ColumnNameCleaner;
+
+		public StringCleaner()
+		{
+			this.FileNameCleaner = new Regex("[^a-zA-Z_]");
+			this.ColumnNameCleaner = new Regex("[^a-zA-Z_]");
+		}
+
+		public string CleanFileName(string FileName)
+		{
+			FileName = FileName.ToLower();
+
+			return this.FileNameCleaner.Replace(FileName, "");
+		}
+
+		public string CleanColumnName(string ColumnName)
+		{
+			ColumnName = ColumnName.ToLower();
+
+			return this.ColumnNameCleaner.Replace(ColumnName, "");
+		}
+	}
+}
