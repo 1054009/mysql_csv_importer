bd2ed519f6cc40a1bc98a40e51542a3b3b795989
efgrthyjkuilo <147861361+1054009@users.noreply.github.com>
2024-03-26 19:09:49 -0400

    More init garbage

--- src/html_parser/html_parser/Form1.cs
+++ src/html_parser/html_parser/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace html_parser
@@ -15,6 +8,11 @@ namespace html_parser
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
