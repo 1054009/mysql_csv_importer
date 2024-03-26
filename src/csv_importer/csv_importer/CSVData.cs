b3385f51e5d78bcbf5420101e7572c62a5a6ceb0
Zach <147861361+1054009@users.noreply.github.com>
2024-03-26 15:56:25 -0400

    Add CVSParser and CVSData class

---
+++ src/csv_importer/csv_importer/CSVData.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace csv_importer
+{
+	public class CSVData
+	{
+		public List<string> ColumnNames { get; protected set; }
+
+		public List<MySQL_Type> ColumnDataTypes { get; protected set; }
+
+		public List<List<string>> RowData { get; protected set; }
+
+		private StringCleaner Cleaner;
+
+		private DataTypes TypeManager;
+
+		public CSVData()
+		{
+			this.ColumnNames = new List<string>();
+			this.ColumnDataTypes = new List<MySQL_Type>();
+			this.RowData = new List<List<string>>();
+
+			this.Cleaner = new StringCleaner();
+			this.TypeManager = new DataTypes();
+
+			this.Clear(); // Redundant
+		}
+
+		public void Clear()
+		{
+			this.ColumnNames.Clear();
+			this.ColumnDataTypes.Clear();
+			this.RowData.Clear();
+		}
+
+		public int AddColumn(string Name)
+		{
+			this.ColumnNames.Add(Name);
+			this.ColumnDataTypes.Add(MySQL_Type.TEXT);
+
+			return this.ColumnNames.Count - 1;
+		}
+
+		public void SetColumnType(int Index, MySQL_Type Type)
+		{
+			if (Index < 0 || Index >= this.ColumnDataTypes.Count)
+				return;
+
+			this.ColumnDataTypes[Index] = Type;
+		}
+
+		public void AverageColumnType(int Index)
+		{
+			if (Index < 0 || Index >= this.ColumnNames.Count)
+				return;
+
+			// Collect all the types in the column
+			List<MySQL_Type> Types = new List<MySQL_Type>();
+
+			foreach (List<string> Row in this.RowData)
+			{
+				string RowColumn = Row[Index];
+
+				Types.Add(this.TypeManager.GetTypeOf(RowColumn));
+			}
+
+			// Find the most common one
+			MySQL_Type Common = Types.GroupBy(Type => Type)
+				.OrderByDescending(Group => Group.Count())
+				.Select(Group => Group.Key)
+				.First();
+
+			// Apply the most common type
+			this.SetColumnType(Index, Common);
+		}
+
+		public void AverageColumnTypes()
+		{
+			for (int i = 0; i < this.ColumnNames.Count; i++)
+				this.AverageColumnType(i);
+		}
+
+		public void AddRow(string[] Row)
+		{
+			List<string> ListRow = new List<string>(Row);
+
+			// Prevent misalignment
+			while (ListRow.Count < this.ColumnNames.Count) ListRow.Add("");
+			while (ListRow.Count > this.ColumnNames.Count) ListRow.RemoveAt(ListRow.Count - 1);
+
+			for (int i = 0; i < ListRow.Count; i++)
+				ListRow[i] = this.Cleaner.FixRowBlock(ListRow[i]);
+
+			this.RowData.Add(ListRow);
+		}
+	}
+}
---
+++ src/csv_importer/csv_importer/CSVParser.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System.IO;
+
+namespace csv_importer
+{
+	public class CSVParser
+	{
+		private CSVData CSV;
+
+		public CSVParser()
+		{
+
+		}
+
+		private void ParseTopRow(string[] Row)
+		{
+			foreach (string Column in Row)
+				this.CSV.AddColumn(Column);
+		}
+
+		private void LoadBasicData(string FilePath)
+		{
+			using (TextFieldParser Parser = new TextFieldParser(FilePath))
+			{
+				Parser.TextFieldType = FieldType.Delimited;
+				Parser.SetDelimiters(",");
+
+				while (!Parser.EndOfData)
+				{
+					string[] Row = Parser.ReadFields();
+
+					// Initialize columns
+					if (this.CSV.ColumnNames.Count < 1)
+					{
+						this.ParseTopRow(Row);
+						continue;
+					}
+
+					// Handle rows
+					this.CSV.AddRow(Row);
+				}
+			}
+		}
+
+		// <<List of Column Names, List of Column Types>, List of <Column Name, Data>>
+		public CSVData HandleFile(string FilePath)
+		{
+			if (!File.Exists(FilePath))
+				return null;
+
+			this.CSV = new CSVData();
+
+			this.LoadBasicData(FilePath);
+
+			this.CSV.AverageColumnTypes();
+
+			return this.CSV;
+		}
+	}
+}
