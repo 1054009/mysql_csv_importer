6152567b0317f5a68111972ddb3401190c462a84
efgrthyjkuilo <147861361+1054009@users.noreply.github.com>
2024-03-26 19:03:22 -0400

    Add main program

--- src/csv_importer/csv_importer/Program.cs
+++ src/csv_importer/csv_importer/Program.cs
@@ -1,11 +1,90 @@
-﻿using System.IO;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.IO;
 using System.Reflection;
+using System.Security;
 
 namespace csv_importer
 {
 	public class Program
 	{
-		static string GetCurrentDirectory()
+		public static string GetUserInput(string Prompt)
+		{
+			Console.Write(Prompt);
+
+			return Console.ReadLine();
+		}
+
+		public static SecureString GetUserInput(string Prompt, bool Secure)
+		{
+			Console.Write(Prompt);
+
+			SecureString Data = new SecureString();
+
+			while (true)
+			{
+				ConsoleKeyInfo KeyInfo = Console.ReadKey(true);
+
+				if (KeyInfo.Key == ConsoleKey.Enter)
+					break;
+
+				if (KeyInfo.Key == ConsoleKey.Backspace)
+				{
+					if (Data.Length > 0)
+					{
+						Data.RemoveAt(Data.Length - 1);
+						Console.Write("\b \b");
+					}
+
+					continue;
+				}
+
+				if (KeyInfo.KeyChar != '\u0000')
+				{
+					Data.AppendChar(KeyInfo.KeyChar);
+					Console.Write("*");
+				}
+			}
+
+			Console.WriteLine();
+
+			return Data;
+		}
+
+		public static void Pause()
+		{
+			Console.WriteLine();
+			Console.WriteLine("Press any key to continue...");
+
+			Console.ReadKey();
+		}
+
+		private static bool HandleConnection(MySqlErrorCode ConnectionCode)
+		{
+			if (ConnectionCode == MySqlErrorCode.Yes)
+			{
+				return true;
+			}
+			else
+			{
+				switch (ConnectionCode)
+				{
+					case MySqlErrorCode.None:
+						Console.WriteLine("Failed to connect to Database. Make sure MySql is running.");
+						break;
+
+					case MySqlErrorCode.AccessDenied:
+						Console.WriteLine("Invalid password supplied.");
+						break;
+				}
+
+				Pause();
+
+				return false;
+			}
+		}
+
+		private static string GetCurrentDirectory()
 		{
 			string Location = Assembly.GetExecutingAssembly().Location;
 
@@ -16,8 +95,17 @@ namespace csv_importer
 		{
 			Importer CSVImporter = new Importer();
 
+			if (!HandleConnection(CSVImporter.ConnectToDatabase()))
+				return;
+
 			CSVImporter.AddFromDirectory(GetCurrentDirectory());
+
 			CSVImporter.LoadFiles();
+			CSVImporter.Import();
+
+			CSVImporter.DisconnectFromDatabase();
+
+			Pause();
 		}
 	}
 }
