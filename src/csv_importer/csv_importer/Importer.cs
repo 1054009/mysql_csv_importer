f3c2c601985d52a5c6a958fe07dc5d9f879570c6
Zach <147861361+1054009@users.noreply.github.com>
2024-03-26 13:37:42 -0400

    Start importer class

---
+++ src/csv_importer/csv_importer/Importer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace csv_importer
+{
+	public class Importer
+	{
+		public Dictionary<string, string> Files { get; protected set; } // <PathToFile, SQL Table Name>
+		private Regex Remover;
+
+		public Importer()
+		{
+			this.Files = new Dictionary<string, string>();
+			this.Remover = new Regex("[^a-zA-Z]");
+		}
+
+		public string CleanString(string Target)
+		{
+			Target = this.Remover.Replace(Target, "");
+			Target = Target.Trim();
+
+			return Target;
+		}
+
+		public string GetFileNameWithoutExtension(string FilePath)
+		{
+			FilePath = Path.GetFileName(FilePath);
+			FilePath = FilePath.ToLower();
+
+			if (!FilePath.EndsWith(".csv"))
+				return string.Empty;
+
+			FilePath = Path.GetFileNameWithoutExtension(FilePath);
+
+			return this.CleanString(FilePath);
+		}
+
+		public void AddFromDirectory(string SearchDirectory)
+		{
+			if (string.IsNullOrEmpty(SearchDirectory)) return;
+			if (!Directory.Exists(SearchDirectory)) return;
+
+			string[] DirectoryEntries = Directory.GetFiles(SearchDirectory);
+			if (DirectoryEntries.Length < 1) return;
+
+			foreach (string DirectoryEntry in DirectoryEntries)
+			{
+				if (!File.Exists(DirectoryEntry)) continue;
+
+				string FileName = this.GetFileNameWithoutExtension(DirectoryEntry);
+
+				if (string.IsNullOrEmpty(FileName)) continue;
+
+				this.Files.Add(DirectoryEntry, FileName);
+			}
+		}
+
+		public void DropFiles()
+		{
+			Files.Clear();
+		}
+	}
+}
