382e1a89078d0d84716b79ab7fdccc09a0be5ede
Zach <147861361+1054009@users.noreply.github.com>
2024-03-26 13:19:55 -0400

    Init project

---
+++ src/csv_importer/csv_importer/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("csv_importer")]
+[assembly: AssemblyDescription("MySQL Mass CSV Import Utility")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("csv_importer")]
+[assembly: AssemblyCopyright("Copyright ©ZS 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
