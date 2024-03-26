a8338843a8484ca9acb18e1954e62a98798db602
efgrthyjkuilo <147861361+1054009@users.noreply.github.com>
2024-03-26 19:08:39 -0400

    Init html parser

---
+++ src/html_parser/html_parser/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("html_parser")]
+[assembly: AssemblyDescription("Parse an HTML table into a CSV file")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("html_parser")]
+[assembly: AssemblyCopyright("Copyright ©ZS 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
