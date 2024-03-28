77d56daaae5f6b78f0fee27344c3663cbc7524a7
efgrthyjkuilo <147861361+1054009@users.noreply.github.com>
2024-03-27 22:50:18 -0400

    Make the length limit as big as it can be
    Why does other stuff keep changing

--- src/html_parser/html_parser/Main.Designer.cs
+++ src/html_parser/html_parser/Main.Designer.cs
@@ -40,10 +40,9 @@
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(-4, 9);
-			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Location = new System.Drawing.Point(-3, 6);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(93, 20);
+			this.label1.Size = new System.Drawing.Size(64, 13);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "HTML Input";
 			// 
@@ -52,13 +51,12 @@
 			this.InputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.InputBox.Location = new System.Drawing.Point(0, 34);
-			this.InputBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.InputBox.MaxLength = 9999999;
+			this.InputBox.Location = new System.Drawing.Point(0, 22);
+			this.InputBox.MaxLength = 2147483647;
 			this.InputBox.Multiline = true;
 			this.InputBox.Name = "InputBox";
 			this.InputBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.InputBox.Size = new System.Drawing.Size(914, 531);
+			this.InputBox.Size = new System.Drawing.Size(610, 347);
 			this.InputBox.TabIndex = 0;
 			this.InputBox.WordWrap = false;
 			// 
@@ -72,13 +70,12 @@
 			this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
 			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-			this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+			this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.tableLayoutPanel1.RowCount = 2;
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 86.66666F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.33333F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(936, 678);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(624, 441);
 			this.tableLayoutPanel1.TabIndex = 3;
 			// 
 			// ConvertButton
@@ -86,10 +83,9 @@
 			this.ConvertButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.ConvertButton.Location = new System.Drawing.Point(10, 588);
-			this.ConvertButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.ConvertButton.Location = new System.Drawing.Point(7, 382);
 			this.ConvertButton.Name = "ConvertButton";
-			this.ConvertButton.Size = new System.Drawing.Size(916, 79);
+			this.ConvertButton.Size = new System.Drawing.Size(610, 52);
 			this.ConvertButton.TabIndex = 0;
 			this.ConvertButton.Text = "Convert to CSV";
 			this.ConvertButton.UseVisualStyleBackColor = true;
@@ -102,20 +98,18 @@
 			this.panel1.Controls.Add(this.label1);
 			this.panel1.Controls.Add(this.InputBox);
 			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.panel1.Location = new System.Drawing.Point(10, 11);
-			this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.panel1.Location = new System.Drawing.Point(7, 7);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(916, 567);
+			this.panel1.Size = new System.Drawing.Size(610, 369);
 			this.panel1.TabIndex = 3;
 			// 
 			// Main
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(936, 678);
+			this.ClientSize = new System.Drawing.Size(624, 441);
 			this.Controls.Add(this.tableLayoutPanel1);
-			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.MinimumSize = new System.Drawing.Size(949, 708);
+			this.MinimumSize = new System.Drawing.Size(638, 474);
 			this.Name = "Main";
 			this.Text = "Main";
 			this.Load += new System.EventHandler(this.Main_Load);
